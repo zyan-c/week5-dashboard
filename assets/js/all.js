84821120a6f5b094855091872e691cf7973f9f80
zyan-c <zyanchang950@gmail.com>
2021-08-07 12:41:05 +0800

    Update 2021-08-07T04:41:03.746Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,8 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+$('.nav-link').click(function (e) {
+  e.preventDefault();
+  $(this).toggleClass('active');
+});
 //# sourceMappingURL=all.js.map
